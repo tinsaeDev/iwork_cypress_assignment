00fc5b589a9272568c2e7d1a18832cc9a43c5ee9
Tinsae B <tinsaedev@gmail.com>
2022-04-15 06:42:26 +0300

    more assertions added

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -7,3 +7,36 @@ define("listeners",function(){
 
     } );
 });
+
+
+define("Search", function(){
+    it( "Empty result for empty query", function(){
+        cy.visit("/");
+        cy.get("#q").clear();
+
+
+        cy.get("#search-result").should(($el) => {
+            expect($el.text().trim()).equal('');
+        });
+
+
+
+    } )
+});
+
+define('Displaying Result', function(){
+    it('Clears results before showing new results', function(){
+
+        let searchQuery = cy.get("#q");
+        searchQuery.type(" ");
+
+       cy.get("#search-result").should(($el) => {
+            expect($el.text().trim()).equal('');
+        });
+
+    })
+
+    it( 'Empty result for not found ', function(){
+
+    } );
+});
